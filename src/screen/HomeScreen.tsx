55cdda1357f667bcc6bb2781479987006e0c997f
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-05-02 18:30:12 +0300

    Text inputs added and keyboard aware scroll view added to handle keyboard visibility and prevent input overlap

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,51 +1,122 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const HomeScreen = () => {
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [textVisible, setTextVisible] = useState(false);
   const handleLogin = () => {
-    Alert.alert("Logging in with:", `${username} ${password}`);
+    if (textVisible) {
+      setPassword("");
+      setUsername("");
+      setTextVisible(false);
+    } else {
+      if (username && password) {
+        setTextVisible(true);
+      } else {
+        Alert.alert("please fill all field");
+      }
+    }
   };
 
   return (
-    <View>
-      <KeyboardAwareScrollView>
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        contentContainerStyle={styles.contentContainer}
+        enableOnAndroid={true}
+        extraScrollHeight={20}
+        keyboardShouldPersistTaps="handled"
+      >
         <Text style={styles.title}>Welcome</Text>
-        <TextInput style={styles.input} placeholder="Username" value={username} onChangeText={setUsername} secureTextEntry/>
-        <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry/>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Log In</Text>
-      </TouchableOpacity>
+          {!textVisible && <Text style={styles.buttonText}>Log In</Text>}
+          {textVisible && <Text style={styles.buttonText}>Log Out</Text>}
+        </TouchableOpacity>
+        {textVisible && (
+          <View style={styles.informationContent}>
+            <Text style={styles.LoginTitle}>Login Information</Text>
+            <Text style={styles.text}>Username: {username}</Text>
+            <Text style={styles.text}>Password: {password}</Text>
+          </View>
+        )}
       </KeyboardAwareScrollView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFF0",
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
   button: {
-    backgroundColor: "#445d7a",
+    backgroundColor: "#513aac",
     paddingVertical: 14,
-    borderRadius: 8,
+    borderRadius: 20,
     alignItems: "center",
-    marginTop: 10,
+    margin: "3%",
   },
   buttonText: {
     color: "white",
     fontWeight: "bold",
     fontSize: 16,
   },
-  title:{
+  title: {
     fontSize: 32,
-    fontWeight:"bold",
-    marginBottom:24,
-    textAlign:"center"
+    fontWeight: "bold",
+    marginBottom: 24,
+    textAlign: "center",
+    color: "#beb1ff",
   },
-  input:{
+  input: {
+    backgroundColor: "white",
+    padding: "3%",
+    margin: "3%",
+    borderWidth: 1,
+    borderColor: "#1b0461",
+    fontSize: 20,
+    borderRadius: 20,
+  },
+  text: {
+    color: "#9998A5",
+    fontSize: 16,
+  },
+  LoginTitle: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+    color: "#9998A5",
+  },
+  informationContent: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-  }
-})
-
-export default HomeScreen
+export default HomeScreen;
